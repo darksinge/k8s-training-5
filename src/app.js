6b3b70d5e24eb5a5be59c2eb9bd825c60653b3fa
Craig Blackburn <cr.blackburn89@gmail.com>
2020-10-19 16:22:04 -0600

    initial commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.engine('.hbs', exphbs({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+
+app.get('/', (req, res) => {
+  return res.render('index')
+})
+
+app.get('/:name', (req, res) => {
+  return res.render('home', {
+    ...req.params
+  })
+})
+
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}`)
+})
+
